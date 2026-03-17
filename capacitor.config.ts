12195b13f5537ef1ffa1907e0972c6ffa25326a4
bdaitravel <info@bdai.travel>
2026-03-17 14:58:13 +0100

    feat: Add html2canvas and types
    Integrates html2canvas for rendering HTML elements to images, along with its corresponding type definitions. This enables new functionalities for image capture within the application.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'travel.bdai.app',
+  appName: 'bdai',
+  webDir: 'dist'
+};
+
+export default config;
